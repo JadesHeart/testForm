d5999c8feae0d8a6166039c7342a6206ee3d8b39
JadesHeart <jadesheartwork@gmail.com>
2022-05-19 22:46:49 +0400

    tabs

--- src/test/java/tests/TabsTest.java
+++ src/test/java/tests/TabsTest.java
@@ -1,5 +1,11 @@
 package tests;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Story;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -19,6 +25,11 @@ public class TabsTest extends BaseTestClass {
         driver.get(ReadProperties.getProperty("browserTabURL"));
     }
 
+    @Description(value = "Тест проверяет что новая вкладка открылась")
+    @Severity(SeverityLevel.CRITICAL)
+    @Epic(value = "Тестирования пользовательского интерфейса")
+    @Feature(value = "Тестирование формы для открытия новой вкладки")
+    @Story(value = "Открытие новой вкладки")
     @Test
     public void testOpeningSecondTab() {
         tabsPage.windowEntry();
@@ -27,6 +38,11 @@ public class TabsTest extends BaseTestClass {
         quantityTabs = switcherWindows.increaseTabCounter(quantityTabs);
     }
 
+    @Description(value = "Тест проверяет что новая вкладка открылась")
+    @Severity(SeverityLevel.CRITICAL)
+    @Epic(value = "Тестирования пользовательского интерфейса")
+    @Feature(value = "Тестирование формы для открытия новой вкладки")
+    @Story(value = "Открытие новой вкладки")
     @Test
     public void testOpeningThirdTab() {
         tabsPage.clickOnNewBrowserTabButton();
