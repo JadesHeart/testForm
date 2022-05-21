15a8fb7c3311a9e164099d107653f34c4a738938
JadesHeart <jadesheartwork@gmail.com>
2022-05-21 10:55:00 +0400

    готово

--- src/test/java/tests/BasicAuthTest.java
+++ src/test/java/tests/BasicAuthTest.java
@@ -1,5 +1,11 @@
 package tests;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Story;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -15,6 +21,11 @@ public class BasicAuthTest extends BaseTestClass {
         driver.get(ReadProperties.getProperty("browserBasicAuthURL"));
     }
 
+    @Description(value = "Тест проверяет на успешную базовую  аутентификация")
+    @Severity(SeverityLevel.CRITICAL)
+    @Epic(value = "Тестирования пользовательского интерфейса")
+    @Feature(value = "Тест формы авторизации")
+    @Story(value = "Авторизация с помощью базовой аутентификации")
     @Test
     public void testBasicAuth() {
         basicAuthPage.clickOnDisplayImage();
